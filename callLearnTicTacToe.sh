135869dc6b1faa224cb4683a7b650d87476cc24f
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2019-04-24 19:30:01 -0400

    callLearnTicTacToe.sh: --numberOfInitialPositions: 256 -> 128    --epsilon=0.3

--- callLearnTicTacToe.sh
+++ callLearnTicTacToe.sh
@@ -6,11 +6,12 @@ python learnTicTacToe.py \
 	--outputDirectory=outputs/ \
 	--proportionOfRandomInitialPositions=0.5 \
 	--maximumNumberOfMovesForInitialPositions=8 \
-	--numberOfInitialPositions=256 \
+	--numberOfInitialPositions=128 \
 	--numberOfGamesForEvaluation=11 \
 	--weightForTheValueLoss=0.1 \
 	--numberOfStandardDeviationsBelowAverageForValueEstimate=0.0 \
 	--softMaxTemperatureForSelfPlayEvaluation=0.1 \
 	--averageTrainingLossToSoftMaxTemperatureForSelfPlayEvaluationDic='{0.4: 0.1, 0.3: 0.1, 0.2: 0.1, 0.1: 0.1}' \
+	--epsilon=0.3 \
 
 
